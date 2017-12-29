5358b2597aa7155a2b66d1fe66338bd577a7376a
fengyuwei <fengyuwei@baidu.com>
2017-12-29 15:14:31 +0800

    Init

---
+++ src/example.cpp
@@ -0,0 +1,14 @@
+#include "example.h"
+
+int Factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+bool IsPrime(int n) {
+    return true;
+}
+
